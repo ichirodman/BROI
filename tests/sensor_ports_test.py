2ebfbe07fed590da3ea157a255ff8e1866b5be65
ichirodman <jsxrodman@gmail.com>
2020-10-08 16:55:24 +0300

    Added path insertion in sensor ports test

--- tests/sensor_ports_test.py
+++ tests/sensor_ports_test.py
@@ -1,5 +1,9 @@
+import sys
 from src.modules.sensors import Sensor
 
-for address in range(0x0, 0xFF):
-    print("Checking port with address : {}. Stand By.".format(address))
-    port = Sensor(address)
+sys.path.insert(0, '../.')
+
+if __name__ == '__main__':
+    for address in range(0x0, 0xFF):
+        print("Checking port with address : {}. Stand By.".format(address))
+        port = Sensor(address)
